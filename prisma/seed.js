af59738a73ba3ba547abb36c506a776909df7c7b
olehmyhal <olehmyhal.pers@gmail.com>
2022-08-04 23:10:57 +0300

    seeding added

--- prisma/seed.js
+++ prisma/seed.js
@@ -2,10 +2,66 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
+const tagSeedData = {
+  title: "new",
+};
+
+const tagSeedData2 = {
+  title: "hot",
+};
+
+const citySeedData = {
+  name: "Kyiv",
+};
+
+const citySeedData2 = {
+  name: "Dnipro",
+};
+
+const promotionSeedData = {
+  image:
+    "https://png.pngtree.com/png-clipart/20200727/original/pngtree-modern-delicious-burger-promotion-poster-png-image_5335151.jpg",
+  title: "Tasty Burger",
+  description: "Tasty Burger with a good price! What can be better?!",
+  createdDiscountAt: "2022-08-01T14:17:32.914Z",
+  endDiscountAt: "2022-09-04T14:17:32.914Z",
+  publishDiscountAt: "2022-07-30T14:17:32.914Z",
+};
+
 const main = async () => {
   await prisma.$connect();
 
-  
+  await prisma.promotionOnCity.deleteMany({});
+  await prisma.promotionOnTag.deleteMany({});
+  await prisma.city.deleteMany({});
+  await prisma.tag.deleteMany({});
+  await prisma.promotion.deleteMany({});
+
+  // await prisma.city.createMany({
+  //   data: [citySeedData, citySeedData2],
+  // });
+  // const cityRes = await prisma.city.findMany({});
+
+  // await prisma.tag.createMany({
+  //   data: [tagSeedData, tagSeedData2],
+  // });
+  // const tagRes = await prisma.tag.findMany({});
+
+  await prisma.promotion.create({
+    data: {
+      ...promotionSeedData,
+      city: {
+        create: [citySeedData, citySeedData2].map((cityVal) => ({
+          city: { create: { name: cityVal.name } },
+        })),
+      },
+      tags: {
+        create: [tagSeedData, tagSeedData2].map((tagVal) => ({
+          tag: { create: { title: tagVal.title } },
+        })),
+      },
+    },
+  });
 
   await prisma.$disconnect();
 };
--- src/database/prisma.service.js
+++ src/database/prisma.service.js
@@ -8,7 +8,7 @@ export class PrismaService {
   async connect() {
     try {
       await this.client.$connect();
-      console.log("[PrismaService] - you succesfully connected to server");
+      console.log("[PrismaService] - you successfully connected to server");
     } catch (e) {
       console.log(
         "[PrismaService] - error while connecting to the server: ",
